8eff564870ac6fdc023d22e2bf41b31edb9b777f
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2016-12-28 16:49:08 +0100

    Add led blinking to visualize transmission errors

--- src/rf95_reliable_datagram_client/rf95_reliable_datagram_client.ino
+++ src/rf95_reliable_datagram_client/rf95_reliable_datagram_client.ino
@@ -12,9 +12,14 @@
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
 
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#define LED 13
+
 // Singleton instance of the radio driver
-RH_RF95 driver;
-//RH_RF95 rf95(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+RH_RF95 driver(RFM95_CS, RFM95_INT);
 
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
@@ -26,19 +31,21 @@ void setup()
 {
   // Rocket Scream Mini Ultra Pro with the RFM95W only:
   // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
 
-  Serial.begin(9600);
-  while (!Serial) ; // Wait for serial port to be available
+  pinMode(LED, OUTPUT);
+
+  //Serial.begin(9600);
+  //while (!Serial) ; // Wait for serial port to be available
   if (!manager.init())
-    Serial.println("init failed");
+    ;//Serial.println("init failed");
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-//  driver.setTxPower(23, false);
+  driver.setTxPower(23, false);
   // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
   // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
@@ -56,7 +63,7 @@ uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  Serial.println("Sending to rf95_reliable_datagram_server");
+  //Serial.println("Sending to rf95_reliable_datagram_server");
     
   // Send a message to manager_server
   if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
@@ -66,18 +73,26 @@ void loop()
     uint8_t from;   
     if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
     {
-      Serial.print("got reply from : 0x");
+      /*Serial.print("got reply from : 0x");
       Serial.print(from, HEX);
       Serial.print(": ");
-      Serial.println((char*)buf);
+      Serial.println((char*)buf);*/
     }
     else
     {
-      Serial.println("No reply, is rf95_reliable_datagram_server running?");
+      //Serial.println("No reply, is rf95_reliable_datagram_server running?");
+      digitalWrite(LED, HIGH);
     }
   }
-  else
-    Serial.println("sendtoWait failed");
+  else {
+    //Serial.println("sendtoWait failed");
+    digitalWrite(LED, HIGH);
+  }
+  delay(5000);
+  digitalWrite(LED, LOW);
   delay(500);
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
 }
 
